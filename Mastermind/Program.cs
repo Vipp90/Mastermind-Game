710604ad0c7ae7edb2c353ae5f98ffb536100d77
Vipp90 <g.zatorski90@wp.pl>
2024-11-04 10:23:18 +0100

    Setup

---
+++ Mastermind/Program.cs
@@ -0,0 +1,16 @@
+using Mastermind;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.Run();
